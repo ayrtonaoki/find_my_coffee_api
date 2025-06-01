76474ef122b52ca2ef6f9e43798501e579543ca3
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-05-31 21:38:30 -0300

    Creates GoogleStores controller

--- app/controllers/api/v1/google_stores_controller.rb
+++ app/controllers/api/v1/google_stores_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::GoogleStoresController < ApplicationController
+  def index
+    google_store_list_service = GetGoogleStoreListService.new(params[:latitude], params[:longitude])
+    stores = google_store_list_service.call
+
+    render json: stores
+  end
 end
