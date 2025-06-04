998821b1a36708ee7535bcbd1d901790304f3b00
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-06-03 21:05:25 -0300

    Creates GoogleStores controller show method

--- app/controllers/api/v1/google_stores_controller.rb
+++ app/controllers/api/v1/google_stores_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::GoogleStoresController < ApplicationController
   def index
     google_store_list_service = GetGoogleStoreListService.new(params[:latitude], params[:longitude])
-    stores = google_store_list_service.call
+    store_list = google_store_list_service.call
 
-    render json: stores
+    render json: store_list
+  end
+
+  def show
+    google_store_details_service = GetGoogleStoreDetailsService.new(params[:id])
+    store_details = google_store_details_service.call
+
+    render json: store_details
   end
 end
