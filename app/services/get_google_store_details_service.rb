e46a157a967f1263c2790fe731da7c71ae3f6516
ayrtonaoki <ayrtonsasouza@gmail.com>
2025-06-03 21:05:13 -0300

    Creates Google store details service

--- app/services/get_google_store_details_service.rb
+++ app/services/get_google_store_details_service.rb
@@ -2,9 +2,20 @@ require 'rest-client'
 require 'json'
 
 class GetGoogleStoreDetailsService
-  def initialize()
+  KEY = "#{Rails.application.credentials.google_secret_key}"
+
+  def initialize(google_place_id)
+    @google_place_id = google_place_id
   end
 
   def call
+    begin
+      base_url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=#{@google_place_id}&key=#{KEY}"
+      response = RestClient.get(base_url)
+
+      JSON.parse(response.body)
+    rescue RestClient::ExceptionWithResponse
+      e.response
+    end
   end
 end
